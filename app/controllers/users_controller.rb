3d847f1c0051c069346da5a1fa2e77d0efd796fb
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-08 10:43:44 -0800

    UserService class now marshalls results to custom types

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,31 +3,31 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @users = UserService.client.get_all_users.map { |user_hash| User.new(user_hash) }
+    @users = UserService.call('get_all_users')
   end
 
   def new
   end
 
   def create
-    user = User.new(UserService.client.create_user(user_params))
+    user = UserService.call('create_user', user_params)
     flash[:notice] = "Successfully created user with name #{user.full_name}"
     redirect_to action: 'index'
   end
 
   def update
-    user = User.new(UserService.client.update_user_by_id(params[:id].to_i, user_params))
+    user = UserService.call('update_user_by_id', params[:id].to_i, user_params)
     flash[:notice] = "Successfully edited user with name #{user.full_name}"
     redirect_to action: 'edit'
   end
 
   def edit
-    @user = User.new(UserService.client.get_user_by_id(params[:id].to_i))
+    @user = UserService.call('get_user_by_id', params[:id].to_i)
   end
 
   def destroy
-    user = User.new(UserService.client.get_user_by_id(params[:id].to_i))
-    UserService.client.delete_user_by_id(user.id)
+    user = UserService.call('get_user_by_id', params[:id].to_i)
+    UserService.call('delete_user_by_id', user.id)
     flash[:notice] = "Successfully deleted user with name #{user.full_name}"
     redirect_to action: 'index'
   end
--- config/initializers/services.rb
+++ config/initializers/services.rb
@@ -1,4 +1,27 @@
 class UserService
+
+  RETURN_MAPPINGS = {
+    'create_user' => {
+      'is_array' => false,
+      'type' => User
+    },
+    'delete_user_by_id' => {
+      'is_array' => false
+    },
+    'get_all_users' => {
+      'is_array' => true,
+      'type' => User
+    },
+    'get_user_by_id' => {
+      'is_array' => false,
+      'type' => User
+    },
+    'update_user_by_id' => {
+      'is_array' => false,
+      'type' => User
+    }
+  }
+
   def self.client
     proxy_service = lambda do
       trans  = Barrister::HttpTransport.new("http://localhost:3001/user_service")
@@ -7,5 +30,25 @@ class UserService
     end
 
     @@service ||= proxy_service.call()
+  end
+
+  def self.call(operation, *args)
+    result = self.client.send operation, *args
+
+    custom_type = RETURN_MAPPINGS[operation]['type']
+
+    if custom_type
+      self.cast operation, result, custom_type, RETURN_MAPPINGS[operation]['is_array']
+    else
+      result
+    end
+  end
+
+  def self.cast(operation, result, type_klass, is_array)
+    if is_array
+      result.map { |hash| type_klass.new(hash) }
+    else
+      type_klass.new(result)
+    end
   end
 end
