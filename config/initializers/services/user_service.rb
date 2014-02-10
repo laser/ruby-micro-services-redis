e2be955fd794a402b1b09d92dafa4cf03e9974c5
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-09 18:29:58 -0800

    Moved UserService initializer into new directory

--- config/initializers/services/user_service.rb
+++ config/initializers/services/user_service.rb
@@ -1,5 +1,14 @@
 class UserService
 
+  class BarristerRpcResult
+    attr_reader :result, :error
+
+    def initialize(result, error)
+      @result = result
+      @error = error
+    end
+  end
+
   RETURN_MAPPINGS = {
     'create_user' => {
       'is_array' => false,
@@ -33,14 +42,17 @@ class UserService
   end
 
   def self.call(operation, *args)
-    result = self.client.send operation, *args
+    begin
+      result = self.client.send operation, *args
+      custom_type = RETURN_MAPPINGS[operation]['type']
 
-    custom_type = RETURN_MAPPINGS[operation]['type']
-
-    if custom_type
-      self.cast operation, result, custom_type
-    else
-      result
+      if custom_type
+        BarristerRpcResult.new(self.cast(operation, result, custom_type), nil)
+      else
+        BarristerRpcResult.new result, nil
+      end
+    rescue Barrister::RpcException => e
+      BarristerRpcResult.new nil, e
     end
   end
 
