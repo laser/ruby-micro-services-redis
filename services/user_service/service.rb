cb293e27ebbeaf7416a41e6d551dd994db4030ac
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-09 18:31:50 -0800

    Service implementation now using exceptions
    * ActiveRecord validation and not-found exceptions are caught and re-thrown as Barrister::RpcException

--- services/user_service/service.rb
+++ services/user_service/service.rb
@@ -1,28 +1,42 @@
 class UserService
-  USER_PROPERTIES = %w(id full_name email phone_number)
+  USER_ATTRIBUTES = %w(id full_name email phone_number)
 
   def get_all_users()
-    User.all.map &method(:to_serializable)
+    call { User.all.map &method(:to_serializable) }
   end
 
   def get_user_by_id(id)
-    to_serializable User.find(id)
+    call { to_serializable User.find(id) }
   end
 
   def delete_user_by_id(id)
-    !!User.destroy(id)
+    call { !!User.destroy(id) }
   end
 
   def update_user_by_id(id, user_properties)
-    to_serializable(User.update(id, user_properties))
+    call do
+      user = User.find(id)
+      user.update!(user_properties)
+      to_serializable user
+    end
   end
 
   def create_user(user_properties)
-    to_serializable(User.create(user_properties))
+    call { to_serializable(User.create!(user_properties)) }
   end
 
 private
   def to_serializable(user_model)
-    user_model.serializable_hash.slice *USER_PROPERTIES
+    user_model.serializable_hash.slice *USER_ATTRIBUTES
+  end
+
+  def call
+    begin
+      yield
+    rescue ActiveRecord::RecordNotFound => e
+      raise Barrister::RpcException.new(101, e.message)
+    rescue ActiveRecord::RecordInvalid => e
+      raise Barrister::RpcException.new(100, e.message)
+    end
   end
 end
