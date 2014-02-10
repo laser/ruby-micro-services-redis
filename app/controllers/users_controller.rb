6b638f811345bc5a188c1be0819837a230af6108
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-09 18:35:18 -0800

    Controllers now plumb BarristerRpc::Exception as strings to the views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,32 +3,46 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @users = UserService.call('get_all_users')
+    @users = UserService.call('get_all_users').result
   end
 
   def new
   end
 
   def create
-    user = UserService.call('create_user', user_params)
-    flash[:notice] = "Successfully created user with name #{user.full_name}"
+    result = UserService.call('create_user', user_params)
+    if result.error
+      flash[:notice] = "Error creating user: #{result.error.message}"
+    else
+      user = result.result
+      flash[:notice] = "Successfully created user with name #{user.full_name}"
+    end
     redirect_to action: 'index'
   end
 
   def update
-    user = UserService.call('update_user_by_id', params[:id].to_i, user_params)
-    flash[:notice] = "Successfully edited user with name #{user.full_name}"
+    result = UserService.call('update_user_by_id', params[:id].to_i, user_params)
+    if result.error
+      flash[:notice] = "Error editing user: #{result.error.message}"
+    else
+      user = result.result
+      flash[:notice] = "Successfully edited user with name #{user.full_name}"
+    end
     redirect_to action: 'edit'
   end
 
   def edit
-    @user = UserService.call('get_user_by_id', params[:id].to_i)
+    @user = UserService.call('get_user_by_id', params[:id].to_i).result
   end
 
   def destroy
-    user = UserService.call('get_user_by_id', params[:id].to_i)
-    UserService.call('delete_user_by_id', user.id)
-    flash[:notice] = "Successfully deleted user with name #{user.full_name}"
+    user   = UserService.call('get_user_by_id', params[:id].to_i).result
+    result = UserService.call('delete_user_by_id', user.id)
+    if result.error
+      flash[:notice] = "Error deleting user: #{result.error.message}"
+    else
+      flash[:notice] = "Successfully deleted user with name #{user.full_name}"
+    end
     redirect_to action: 'index'
   end
 
@@ -36,5 +50,4 @@ private
   def user_params
     params.require(:user).permit(:full_name, :phone_number, :email)
   end
-
 end
