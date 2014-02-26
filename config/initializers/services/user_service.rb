0ade443bbd036c59768de1fdfacb1e0337c0324a
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-25 16:40:29 -0800

    Lazy-load the Rails client

--- config/initializers/services/user_service.rb
+++ config/initializers/services/user_service.rb
@@ -1,9 +1,13 @@
 class UserService
-  config = YAML::load_file(File.join(Rails.root, 'config', 'redis.yml'))['default']
-
-  @@instance = Barrister::Rails::Client.new(Barrister::Transports::Redis.new(config['list_name'], ENV['OPENREDIS_URL'] || config['database_url']))
 
   def self.instance
+    @@instance ||= create
     @@instance.UserService
   end
+
+  def self.create
+    config = YAML::load_file(File.join(Rails.root, 'config', 'redis.yml'))['default']
+    Barrister::Rails::Client.new(Barrister::Transports::Redis.new(config['list_name'], ENV['OPENREDIS_URL'] || config['database_url']))
+  end
+
 end
