5c17cdf19f9f34b5a307e6de1a5cc8e870c277aa
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-03-05 09:40:07 -0800

    Updating constructor invocation to use new options hash

--- services/user_service/app.rb
+++ services/user_service/app.rb
@@ -1,5 +1,5 @@
 require 'barrister-redis'
 require './service.rb'
 
-container = Barrister::RedisContainer.new('./user_service.json', ENV['OPENREDIS_URL'] || 'redis://localhost:6379', 'foo_bar', [UserService])
+container = Barrister::RedisContainer.new('./user_service.json', UserService.new, url: ENV['OPENREDIS_URL'] || 'redis://localhost:6379', list_name: 'foo_bar')
 container.start
